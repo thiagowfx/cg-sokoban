8b49588940a667403bf9ff0e4137e9ab187ead02
Thiago Barroso Perrotta <thiagoperrotta95@gmail.com>
2014-10-28 00:18:45 -0200

    from sdl to sdl2

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "SDL.h"
 using namespace std;
 
 int main() {
   std::cout << "Hello world!" << std::endl;
+
+  SDL_Init(SDL_INIT_HAPTIC);
+  SDL_Quit();
 
   return 0;
 }
