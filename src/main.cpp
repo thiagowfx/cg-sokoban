3fbec73e2e1cadc3cc0363b34a8f2bdcf5bc0f6f
Pedro Cruz <pcruz@inoa.com.br>
2014-11-25 11:05:21 -0200

    small routine in order to test the game by printing it on ASCII characters. So funny.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
+#include <iostram>
 #include "gui.hpp"
 
 int main(int argc, char** argv) {
   Gui *gui = new Gui();
   gui->gameLoop();
   return EXIT_SUCCESS;
+}
+
+void testGameLogic() {
+  SokoBoard board("map1.soko");
+  bool go = true;
+  char command;
+  while(go) {
+    cout << board.toString();
+    cout << endl << "======== HELP ========" << endl;
+    cout << endl << "up: w; down: s; right: d; left: a " << endl;
+    cin >> command;
+    switch(command) {
+    case 'w':
+      board.move(Direction::DOWN);
+      break;
+    case 's':
+      board.move(Direction::UP);
+      break;
+    case 'd':
+      board.move(Direction::RIGHT);
+      break;
+    case 'a':
+      board.move(Direction::LEFT);
+      break;
+    case 'e':
+      go = false;
+      break;
+    default:
+      break;
+    }
+  }
+  system("pause");
+  return 0;
 }
