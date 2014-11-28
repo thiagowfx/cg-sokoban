2b968db71879338541257e57351c1dcf4de60236
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-11-28 02:04:00 -0200

    add usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 #include "gui.hpp"
 
+/// This function starts the game in ASCII mode. Useful for testing, but not so funny. =/
 void startASCIIMode();
 
+/// Print useful information about the game.
+void usage() {
+  std::cout << ".----------------. .----------------. .----------------. .----------------. \n| .--------------. | .--------------. | .--------------. | .--------------. |\n| | _____  _____ | | |  _________   | | |  _______     | | |     _____    | |\n| ||_   _||_   _|| | | |_   ___  |  | | | |_   __ \\    | | |    |_   _|   | |\n| |  | |    | |  | | |   | |_  \\_|  | | |   | |__) |   | | |      | |     | |\n| |  | '    ' |  | | |   |  _|      | | |   |  __ /    | | |   _  | |     | |\n| |   \\ `--' /   | | |  _| |_       | | |  _| |  \\ \\_  | | |  | |_' |     | |\n| |    `.__.'    | | | |_____|      | | | |____| |___| | | |  `.___.'     | |\n| |              | | |              | | |              | | |              | |\n| '--------------' | '--------------' | '--------------' | '--------------' |\n '----------------' '----------------' '----------------' '----------------' " << std::endl;
+  std::cout << "Developed by:" << std::endl;
+  std::cout << "\tThiago Barroso Perrotta" << std::endl;
+  std::cout << "\tFelipe Podolan Oliveira" << std::endl;
+  std::cout << "\tPedro Cruz Caminha" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Copyright (C) 2014" << std::endl;
+  std::cout << "This program is distributed under a MIT License." << std::endl;
+  std::cout << "Its source code is available at:" << std::endl;
+  std::cout << "\t" << "https://github.com/thiagowfx/cg-sokoban" << std::endl;
+}
+
 int main(int argc, char** argv) {
+  if (argc == 2 && (!strcmp(argv[1], "-h") || !strcmp(argv[1], "--help") || !strcmp(argv[1], "-v"))) {
+    usage();
+    return EXIT_SUCCESS;
+  }
+
   Gui *gui = new Gui();
   gui->gameLoop();
   // startASCIIMode();
   return EXIT_SUCCESS;
 }
 
-/// This function starts the game in ASCII mode. Useful for testing, but not so funny =/
 void startASCIIMode() {
-  SokoBoard board("map1.soko");
+  SokoBoard board("../assets/stages/stage1.sok");
   bool go = true;
   char command;
   while(go) {
